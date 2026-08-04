efe8633c2d7c4bd5b2122e8bc042c5cc263975ce
IcecreamCamera <wnsdyd029451@gmail.com>
2026-08-04 17:50:30 +0900

    필요없는거없애고주석처리

--- skala-vue/src/App.vue
+++ skala-vue/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import WeatherMockup from './components/exercise/WeatherMockup.vue'
+/* import WeatherMockup from './components/exercise/WeatherMockup.vue'
 import WeatherComposition from './components/exercise/WeatherComposition.vue'
-import WeatherParent from './components/exercise/WeatherParent.vue'
+import WeatherParent from './components/exercise/WeatherParent.vue' */
 import UnitToggler from './components/exercise/UnitToggler.vue'
 </script>
 
 <template>
-  <div class="app-container">
+  <!--<div class="app-container">
     <h1>⛅ 과제 1: 날씨 (Mockup)</h1>
     <hr />
     <WeatherMockup />
@@ -32,14 +32,14 @@ import UnitToggler from './components/exercise/UnitToggler.vue'
       </nav>
       <main>
         <RouterView />
-        <!-- RouterView v-slot="{ Component }">
+        <RouterView v-slot="{ Component }">
           <KeepAlive>
             <component :is="Component" />
           </KeepAlive>
-        </RouterView-->
+        </RouterView>
       </main>
     </div>
-  </div>
+  </div>   -->
   <div class="app-container">
     <h1>⛅ 과제 5: 스토어적용</h1>
     <hr />
